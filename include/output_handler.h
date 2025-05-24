002e6fdf92d94b8e2189897c432f71eabce124e6
Huiseong Lim <limhuiseong.cs@gmail.com>
2025-05-24 06:08:07 +0000

    Add print_prompt function

---
+++ include/output_handler.h
@@ -0,0 +1,8 @@
+#ifndef OUTPUT_HANDLER_H_
+#define OUTPUT_HANDLER_H_
+
+#include <stdio.h>
+
+void print_prompt();
+
+#endif
---
+++ tokenizer/output_handler.c
@@ -0,0 +1,6 @@
+#include "output_handler.h"
+
+void print_prompt()
+{
+    printf("forest-db > ");
+}
